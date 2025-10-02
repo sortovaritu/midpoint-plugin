2c6c6f4e2a0e49e9160a575838c8ed8d6df611f1
Dzmitry Rozberh <Dzmitry_Rozberh@epam.com>
2025-10-02 11:13:45 +0200

    update

---
+++ src/WinFormsQtBridge.Plugin/Infrastructure/Services/PipeServerService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using WinFormsQtBridge.Plugin.Infrastructure.Services.Interfaces;
+
+namespace WinFormsQtBridge.Plugin.Infrastructure.Services
+{
+    public class PipeServerService : IPipeServerService
+    {
+        private const string PipeName = "WinFormsQtBridge.PipeServerService";
+        
+        public Func<string, string> OnActionRecieved { get; set; }
+
+        public void Start()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    using (var server = new NamedPipeServerStream(PipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Message))
+                    {
+                        await server.WaitForConnectionAsync();
+
+                        using (var reader = new StreamReader(server))
+                        using (var writer = new StreamWriter(server))
+                        {
+                            writer.AutoFlush = true;
+
+                            while (server.IsConnected)
+                            {
+                                var message = await reader.ReadLineAsync();
+
+                                if (message == null)
+                                {
+                                    break;
+                                }
+
+                                var response = OnActionRecieved?.Invoke(message);
+
+                                await writer.WriteLineAsync(response);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}

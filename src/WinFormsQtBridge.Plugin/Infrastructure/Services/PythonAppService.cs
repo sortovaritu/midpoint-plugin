fd3dbb32bb8c60c210dcbaa4b631f9b696699099
Dzmitry Rozberh <Dzmitry_Rozberh@epam.com>
2025-10-03 14:15:16 +0200

    update

--- src/WinFormsQtBridge.Plugin/Infrastructure/Services/PythonAppService.cs
+++ src/WinFormsQtBridge.Plugin/Infrastructure/Services/PythonAppService.cs
@@ -8,25 +8,17 @@ namespace WinFormsQtBridge.Plugin.Infrastructure.Services
     {
         public void StartPythonApp()
         {
-            const string pythonExe = @"python-script\venv\Scripts\python.exe";
-            const string scriptPath = @"python-script\main.py";
-            
-            if (!System.IO.File.Exists(pythonExe))
-            {
-                IP_Message.Error("Unable to find python.exe file");
-                return;
-            }
+            const string scriptPath = @"python-script\main.exe";
 
             if (!System.IO.File.Exists(scriptPath))
             {
-                IP_Message.Error("Unable to find script file main.py");
+                IP_Message.Error("Unable to find script file main.exe");
                 return;
             }
 
             var psi = new ProcessStartInfo
             {
-                FileName = pythonExe,
-                Arguments = $"\"{scriptPath}\"",
+                FileName = scriptPath,
                 UseShellExecute = false,
                 CreateNoWindow = true,
                 RedirectStandardOutput = true,
--- src/WinFormsQtBridge.Plugin/python-script/main.py
+++ src/WinFormsQtBridge.Plugin/python-script/main.py
@@ -2,7 +2,7 @@ import sys
 import win32file
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit
 
-PIPE_NAME = r'\\.\pipe\PluginPipe'
+PIPE_NAME = r'\\.\pipe\WinFormsQtBridge.PipeServerService'
 
 def send_message(message):
     try:
@@ -13,13 +13,13 @@ def send_message(message):
             win32file.OPEN_EXISTING,
             0, None
         )
-        # отправляем сообщение
+
         win32file.WriteFile(handle, (message + "\n").encode('utf-8'))
-        # читаем ответ
+
         resp = win32file.ReadFile(handle, 4096)[1]
         response_text = resp.decode('utf-8')
 
-        # отображаем в edit поле
+
         response_edit.setText(response_text)
 
         handle.Close()
@@ -33,18 +33,18 @@ window.setWindowTitle("Qt NamedPipe Client")
 
 layout = QVBoxLayout()
 
-# Кнопки
+
 btn1 = QPushButton("Button1")
 btn1.clicked.connect(lambda: send_message("Button1"))
 
 btn2 = QPushButton("Button2")
 btn2.clicked.connect(lambda: send_message("Button2"))
 
-# Поле для отображения ответа
+
 response_edit = QLineEdit()
 response_edit.setReadOnly(True)
 
-# Добавляем элементы в layout
+
 layout.addWidget(btn1)
 layout.addWidget(btn2)
 layout.addWidget(response_edit)
